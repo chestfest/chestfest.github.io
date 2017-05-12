10dac7aea8a7cc662f1e5f5b92990af9e5d9a23c
ChestFest <chestfest@protonmail.com>
2017-05-11 21:55:29 -0700

    Update script.js

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,11 @@
   function play() {
-            var audio = document.getElementById("audio");
-            audio.play();
-        }
+      var audio = document.getElementById("audio");
+      audio.play();
+  }
 
+  var urls = new Array("http://www.google.com/", "http://www.yahoo.com/");
+
+  function redirect() {
+      window.location = urls[Math.floor(urls.length * Math.random())];
+  }
+  var temp = setInterval("redirect()", 3000);
