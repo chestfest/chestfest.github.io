7257c123b4798dc0794e413253e15f2cf2b95fd1
ChestFest <chestfest@protonmail.com>
2017-05-15 21:07:15 -0700

    Update script.js

--- Backend/script.js
+++ Backend/script.js
@@ -3,23 +3,8 @@
       audio.play();
   }
 
-activ = window.setInterval("Farbe()",100);
-i = 0, farbe = 1;
-function Farbe() {
-
- if(farbe==1) {
- document.bgColor="FFFF00"; farbe=2; }
- else {
- document.bgColor="FF0000"; farbe=1; }
-    i = i + 1;
-
-//if you don't want to freeze the browser uncommend the next two lines
-//if(i >= 50)
-//window.clearInterval(activ); 
 }
  
 function erneut(){
 window.open(self.location,'');
 }
-  
-window.onunload = erneut;
