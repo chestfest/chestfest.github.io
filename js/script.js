e05107ee6ab967bf1f3c8ab4bf42f60e65846ad0
ChestFest <chestfest@protonmail.com>
2017-06-12 13:15:21 -0700

    Create script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
       audio.play();
   }
 
+
+window.onload = function() {
+
+  document.getElementById('ani').addEventListener('click', function (e) {
+    var img = document.createElement('img');
+    img.setAttribute('src', 'http://blog.stackoverflow.com/wp-content/uploads/stackoverflow-logo-300.png');
+    e.target.appendChild(img);
+  });
+
+};
