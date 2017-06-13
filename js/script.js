20117a3cd890592b71010029ccc24ce284b233dc
ChestFest <chestfest@protonmail.com>
2017-06-13 14:36:36 -0700

    Create script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,14 @@
-var s = new Audio('Display/hit.wav');
-
-var s1 = new Audio('Display/punch.wav');
-
-var playFirst = true;
-function play() {
-  if (playFirst) {
-    s.play();
-  } else {
-    s2.play();
+  function play() {
+      var audio = document.getElementById("audio");
+      audio.play();
   }
-  playFirst = !playFirst;
-}
 
+  window.onload = function() {
 
-window.onload = function() {
+      document.getElementById('ani').addEventListener('click', function(e) {
+          var img = document.createElement('img');
+          img.setAttribute('src', 'http://blog.stackoverflow.com/wp-content/uploads/stackoverflow-logo-300.png');
+          e.target.appendChild(img);
+      });
 
-  document.getElementById('ani').addEventListener('click', function (e) {
-    var img = document.createElement('img');
-    img.setAttribute('src', 'http://blog.stackoverflow.com/wp-content/uploads/stackoverflow-logo-300.png');
-    e.target.appendChild(img);
-  });
-
-};
+  };
