0520c85b189fccd4c4f38855809af252da57e338
ChestFest <chestfest@protonmail.com>
2017-06-14 21:12:48 -0700

    Create script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@
       audio.play();
   }
 
+var clickSound = new Audio("Display/Audio/Airhorn.wav");
+
+function playAudioOnKeyDown(e) {
+  if (e.keyCode == 38) {
+    clickSound.play();
+  }
+}
+document.onkeydown = playAudioOnKeyDown;
+
+
+function pauseAudioOnKeyUp(e) {
+  if (e.keyCode == 38) {
+    clickSound.pause();
+  }
+}
+document.onkeyup = pauseAudioOnKeyUp;
+
   window.onload = function () {
       document.getElementById('ani').addEventListener('click', function (e) {
           var img = document.createElement('img');
