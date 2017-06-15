24ce74e20dd0be0a316e195c109c2a73e3837a6e
ChestFest <chestfest@protonmail.com>
2017-06-14 20:45:16 -0700

    Create script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,29 @@
-  function play(){
-       var audio = document.getElementById("play()");
-       audio.play();
-                 }
+  function play() {
+      var audio = document.getElementById("play()");
+      audio.play();
+  }
 
-    $(document).ready(function() {
-        var obj = document.createElement("audio");
-        obj.src="Display/Audio/Airhorn.wav";
-        obj.volume=0.10;
-        obj.autoPlay=false;
-        obj.preLoad=true;       
- 
-        $(".playSound").click(function() {
-            obj.play();
-        });
- 
-    });
- 
+  function playverify() {
+      play();
+  }
 
-  window.onload = function() {
+  $(document).ready(function () {
+      var obj = document.createElement("audio");
+      obj.src = "Display/Audio/Airhorn.wav";
+      obj.volume = 0.10;
+      obj.autoPlay = false;
+      obj.preLoad = true;
 
-      document.getElementById('ani').addEventListener('click', function(e) {
+      $(".playSound").click(function () {
+          obj.play();
+      });
+
+  });
+
+
+  window.onload = function () {
+
+      document.getElementById('ani').addEventListener('click', function (e) {
           var img = document.createElement('img');
           img.setAttribute('src', 'http://blog.stackoverflow.com/wp-content/uploads/stackoverflow-logo-300.png');
           e.target.appendChild(img);
