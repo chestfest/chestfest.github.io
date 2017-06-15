48aabb549bea0e69ceaa1e573a0b61935390dd36
ChestFest <chestfest@protonmail.com>
2017-06-14 20:54:48 -0700

    Create script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,14 @@
       });
 
   };
+
+  function play_audio() {
+      ///alert('hi');
+      //$('#id1').get(0).play()
+      //var snd = new Audio("Display/Audio/Airhorn.wav"); // buffers automatically when created
+      //document.getElementById('AudioMain').innerHTML = "";
+      //for(var ni=1;ni<=12;ni++)
+      //{
+      //alert(ni);
+      //document.getElementById('AudioMain').innerHTML+=' <audio id="id'+ni+'" preload="auto"><source src="Display/Audio/Airhorn.wav" type="audio/wav"><source src="Display/Audio/Airhorn.wav" type="audio/wav"></audio>';
+      //}
