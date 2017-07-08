94d10e9fbbd0d1f9bd5f9ac4ddece8b1549a7f4c
ChestFest <chestfest@protonmail.com>
2017-07-08 14:16:26 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,1 @@
-  function play() {
-      var audio = document.getElementById("play()");
-      audio.play();
-  }
-
-  document.getElementById("chest").onclick = function chest() {
-      var audio = document.getElementById("play()");
-      audio.play();
-  }
+'use strict'
