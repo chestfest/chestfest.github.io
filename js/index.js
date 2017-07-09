b8ced45de6e5d7ee26afcb7c03d6f2d8fa211458
ChestFest <chestfest@protonmail.com>
2017-07-09 10:23:29 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
 'use strict'
 
-document.getElementById("inner_1").onclick = function() {
-    alert("Ow!")
+navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+  var video = document.querySelector('video');
+  video.src = window.URL.createObjectURL(localMediaStream);
+
+  // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+  // See crbug.com/110938.
+  video.onloadedmetadata = function(e) {
+  // Ready to go. Do some stuff.
+  };
+}, onFailSoHard);
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+var video = document.querySelector('video');
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia({audio: true, video: true}, function(stream) {
+    if (navigator.webkitGetUserMedia) {
+      video.src = window.webkitURL.createObjectURL(stream);
+    } else {
+      video.src = stream; // Opera
+    }
+  }, onFailSoHard);
+} else {
+  video.src = 'somevideo.webm'; // fallback.
+}
+
+function hasGetUserMedia() {
+   // Note: Opera builds are unprefixed.
+   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia);
 }
