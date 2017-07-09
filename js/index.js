76089420dceb62dd2e6046c3cf07c240fbc96a29
ChestFest <chestfest@protonmail.com>
2017-07-09 14:07:22 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,1 @@
 'use strict'
-
-// Grab elements, create settings, etc.
-var video = document.getElementById('video');
-
-// Get access to the camera!
-if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-    });
-}
-
-// Elements for taking the snapshot
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-var video = document.getElementById('video');
