b41e39a1818d682f469131d706b26fea1bf37422
ChestFest <chestfest@protonmail.com>
2017-07-10 09:15:31 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 'use strict'
 //using sweetalert code here
-swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
+$(function() {
+    if ($('body').is('.alert')) {
+        swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
+    }
+});
