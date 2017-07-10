f153f60bce9450f114c260b45bb4bbbb4e3f4e43
ChestFest <chestfest@protonmail.com>
2017-07-10 09:46:15 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'use strict'
 //using sweetalert code here
-if (window.location.pathname == '#') {
+if (window.location.pathname == 'https://chestfest.github.io/') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
 }
