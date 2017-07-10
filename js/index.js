46c235cf05b9b4d14bf59970b7add46c3bd8fbee
ChestFest <chestfest@protonmail.com>
2017-07-10 09:30:07 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'use strict'
 //using sweetalert code here
-if (window.location.pathname == '/podcast') {
+if (window.location.pathname == '#') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
 }
