30e2f005de191f8c3c958ecd50bef58cc3215d32
ChestFest <chestfest@protonmail.com>
2017-07-09 21:53:41 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 'use strict'
+//using sweetalert code here
 
-swal("Roast", "I'm useless. At least you're alive. Too bad you can't do anything with your life.")
+function alert {
+    swal("Roast", "I'm useless. At least you're alive. Too bad you can't do anything with your life.")
+}
