ccb4518b0e91bba75f41bffbcc7ad13d291bbd1f
ChestFest <chestfest@protonmail.com>
2017-07-10 17:56:07 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,3 @@
 if (window.location.pathname != '/c/web') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
 }
-
-if (window.location.pathname != '/c/web.html') {
-    swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
-}
