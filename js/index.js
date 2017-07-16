a0b663d95ed0a66276ee9fa827f1fe5b0dec9f83
ChestFest <chestfest@protonmail.com>
2017-07-16 13:35:26 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 'use strict'
+console.log('Welcome to ChestFest, a website unblocker program. You can find the Github Repo here: https://github.com/chestfest/chestfest.github.io')
 //using sweetalert code here
 if (window.location.pathname != '/c/web') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
+    console.log('Debug message for swal')
 }
 
-console.log('Welcome to ChestFest, a website unblocker program. You can find the Github Repo here: https://github.com/chestfest/chestfest.github.io')
