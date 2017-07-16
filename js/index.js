71caab990bd124d6b6df863238890e65d6687f9a
ChestFest <chestfest@protonmail.com>
2017-07-16 13:51:54 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,5 +4,6 @@ if (window.location.pathname != '/c/web') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
 }
 console.log('Welcome to ChestFest, a website unblocker program. You can find the Github Repo here: https://github.com/chestfest/chestfest.github.io')
-console.log('-----')
-console.log('v1.0b')
+window.onload = function debug() {
+    console.log(' - = + L o a d e d + = -')
+}
