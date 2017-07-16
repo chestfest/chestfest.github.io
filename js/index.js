03a619409f4e7617d744de95cb7a8bb32c605b86
ChestFest <chestfest@protonmail.com>
2017-07-16 15:46:32 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 if (window.location.pathname != '/c/web') {
     swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
 }
-console.log('%c              ChestFest', 'color: #8CD4F5; font-size: 20px; font-weight: bold;')
+console.log('%c                              ChestFest', 'color: #8CD4F5; font-size: 20px; font-weight: bold;')
 window.onload = function debug() {
     console.info('Welcome to ChestFest, a website unblocker program. You can find the Github Repo here: https://github.com/chestfest/chestfest.github.io')
 }
