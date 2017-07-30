285a829a530494fbbd34d57b02dc60a246b4f0f8
ChestFest <chestfest@protonmail.com>
2017-07-30 09:20:05 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,11 @@ window.onload = function debug() {
 window.onerror = function() {
     console.error('Try to reload (⟳) page. If page still does not work, wait until our developers fix the problem.')
 }
+
+f (cursorChar === ' ') {
+        document.title = document.title.slice(0, document.title.length-1);
+    } else {
+        document.title += cursorChar;
+    } 
+    updateLine();
+}
