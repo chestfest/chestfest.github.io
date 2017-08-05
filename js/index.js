4724dafa0d1da4d3615701e5336d489b635dd047
ChestFest <chestfest@protonmail.com>
2017-08-05 14:12:18 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ if (window.location.pathname != '/c/web') {
 }
 
 window.onload = function () {
-    document.getElementById("submit").onclick = function() {
-    document.getElementById("web").innerhtml += '<iframe src="' + website + '" id="web" style="position:fixed; top:80px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>';
+    //document.getElementById("submit").onclick = function() {
+    document.getElementById("web").innerhtml += '<iframe src="' + website + '" style="position:fixed; top:80px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>';
 }
-}
+//}
 
 
