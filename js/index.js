2e6b5b2c4571f8710124fc90f82a3a6628d6bf90
ChestFest <chestfest@protonmail.com>
2017-08-16 23:14:16 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,8 +10,8 @@ window.onload = function() {
 function KeyPress(e) {
     var cf = window.event ? event : e
 
-    if (cf.keyCode == 116 && cf.ctrlKey) swal({title: "Refreshing...", showConfirmButton: false});
-    else if (cf.keyCode == 116) swal({title: "Refreshing...", showConfirmButton: false});
+    if (cf.keyCode == 116 && cf.ctrlKey) swal({title: "Refreshing...", text: "App Created by Inb4", showConfirmButton: false});
+    else if (cf.keyCode == 116) swal({title: "Refreshing...", text: "App Created by Inb4", showConfirmButton: false});
 }
 
 document.onkeydown = KeyPress;
