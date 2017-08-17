59def1af8c023e785b4a46369fc5df8a3c435ded
ChestFest <chestfest@protonmail.com>
2017-08-16 23:24:02 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 //using sweetalert code here
 window.onload = function() {
-    if (window.location.pathname != '/c/web') {
+    if (window.location.pathname != '/c/splix-io') {
         swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
     }
 }
@@ -10,9 +10,9 @@ window.onload = function() {
 function KeyPress(e) {
     var cf = window.event ? event : e
 
-    if (cf.keyCode == 116 && cf.ctrlKey) swal({title: "Refreshing...", text: "Will be done in a sec!", showConfirmButton: false});
-    else if (cf.keyCode == 116) swal({title: "Refreshing...", text: "Will be done in a sec!", showConfirmButton: false});
-    else if (cf.keyCode == 82 && cf.ctrlKey) swal({title: "Refreshing...", text: "Will be done in a sec!", showConfirmButton: false});
+    if (cf.keyCode == 116 && cf.ctrlKey) swal({title: "Refreshing...", text: "┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻", showConfirmButton: false});
+    else if (cf.keyCode == 116) swal({title: "Refreshing...", text: "┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻", showConfirmButton: false});
+    else if (cf.keyCode == 82 && cf.ctrlKey) swal({title: "Refreshing...", text: "┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻", showConfirmButton: false});
 }
 
 document.onkeydown = KeyPress;
