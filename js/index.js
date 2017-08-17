3f2044876bbe706b0f5136973c33c413fa318256
ChestFest <chestfest@protonmail.com>
2017-08-16 22:56:51 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@
     //if (window.location.pathname != '/c/web') {
         swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
     //}
+
+        function KeyPress(e) {
+            var cf = window.event? event : e
+
+            if (cf.keyCode == 116 && cf.ctrlKey) alert("Goodbye!");
+        }
+
+        document.onkeydown = KeyPress;
