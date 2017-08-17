1ec8b1212b3aafc54d4b9c52f8947fc5d938eef2
ChestFest <chestfest@protonmail.com>
2017-08-16 23:10:37 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function KeyPress(e) {
     var cf = window.event ? event : e
 
     if (cf.keyCode == 116 && cf.ctrlKey) swal({title: "Refreshing...", showConfirmButton: false});
-    else if (cf.keyCode == 116) swal({title: "Refreshing...", showConfirmButton: false);
+    else if (cf.keyCode == 116) swal({title: "Refreshing...", showConfirmButton: false});
 }
 
 document.onkeydown = KeyPress;
