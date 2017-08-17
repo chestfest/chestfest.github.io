14e9a7aa72283e38c93d4bf5bf5620414793aa9a
ChestFest <chestfest@protonmail.com>
2017-08-16 22:58:55 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 'use strict'
 
 //using sweetalert code here
-    //if (window.location.pathname != '/c/web') {
-        swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
+//if (window.location.pathname != '/c/web') {
+swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
     //}
 
-        function KeyPress(e) {
-            var cf = window.event? event : e
+function KeyPress(e) {
+    var cf = window.event ? event : e
 
-            if (cf.keyCode == 116 && cf.ctrlKey) alert("Goodbye!");
-        }
+    if (cf.keyCode == 116 && cf.ctrlKey) swal("Refreshing...");
+    else if (cf.keyCode == 116) swal("Refreshing...");
+}
 
-        document.onkeydown = KeyPress;
+document.onkeydown = KeyPress;
