714cc3358e10e0528687eecf704ac7962fa958d8
ChestFest <chestfest@protonmail.com>
2017-09-01 12:16:30 -0700

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+//using sweetalert code here
+window.onload = function() {
+    if (window.location.pathname != '/c/splix-io') {
+        swal("Notice", "This program is in the developing stage and some assets may be unfinished or broken.")
+    }
+}
+
+function KeyPress(e) {
+    var cf = window.event ? event : e
+
+    if (cf.keyCode == 116 && cf.ctrlKey) swal({
+        title: "Refreshing...",
+        showConfirmButton: false
+    });
+    else if (cf.keyCode == 116) swal({
+        title: "Refreshing...",
+        showConfirmButton: false
+    });
+    else if (cf.keyCode == 82 && cf.ctrlKey) swal({
+        title: "Refreshing...",
+        showConfirmButton: false
+    });
+}
+
+document.onkeydown = KeyPress;
